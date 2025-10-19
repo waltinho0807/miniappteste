c13a61bbdf5061b5b9c6bb4b7073b76037fc57bc
waltinho0807 <waltinhocalegari@hotmail.com>
2025-10-19 11:43:22 -0300

    manifest

--- app/components/TonConnectProviderWrapper.tsx
+++ app/components/TonConnectProviderWrapper.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 export function TonConnectProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     //<TonConnectUIProvider manifestUrl="https://uncritically-unadducible-gael.ngrok-free.dev/tonconnect-manifest.json">
-    <TonConnectUIProvider manifestUrl="https://miniappteste.vercel.app/">
+    <TonConnectUIProvider manifestUrl="https://miniappteste.vercel.app/tonconnect-manifest.json">
       {children}
     </TonConnectUIProvider>
   );
